65f7c138c7fb681949b3f6761fbeb96452d3f9e5
Kyle Horsley <35946547+KyleRoger@users.noreply.github.com>
2019-11-10 17:30:20 -0500

    Adding project

---
+++ ConestogaCollegeBookStore/Models/BooksViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConestogaCollegeBookStore.Models
+{
+    public class BooksViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Program { get; set; }
+        public decimal Price { get; set; }
+    }
+}
